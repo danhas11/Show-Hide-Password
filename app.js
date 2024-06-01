34fa158607c1786d508ee98ec382d628f0dc3e80
Hassan Shams <138686748+danhas11@users.noreply.github.com>
2024-06-01 14:10:58 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const input = document.querySelector('#pass')
+const eye = document.querySelector('#eye-icon')
+
+eye.addEventListener('click' , ()=>{
+    // if(input.type === 'password'){
+    //     input.type = 'text';
+    // }
+    // else{
+    //     input.type = 'password';
+    // }
+
+    input.type === 'password' ? (input.type = 'text') : (input.type = 'password');
+})
